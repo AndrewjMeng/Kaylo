94491645d64a4e937f11b90fe21a66274d63d7fc
andrewjmeng@gmail.com <andrewjmeng@gmail.com>
2016-08-22 19:10:26 -0700

    functioning need to remove inputs after submit

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-var weatherCall = function(urls, zipOrCity) {
+let weatherCall = (urls, zipOrCity) => {
     $.ajax({
     url : urls,
     dataType : "json",
-    success : function(url) {
+    success : url => {
       console.log(url)
       addToDom(url);
       }
     });
-  };
-  var startSearch = function() {
-  var queryCity = document.getElementById('city').value;
-  var queryState = document.getElementById('state').value;
-  var queryZip = document.getElementById('zipcode').value;
-  var urls = '';
-  var zipOrCity = false;
+};
+let autoLocation = () => {
+  urls = "http://api.wunderground.com/api/0c6827c56d281db1/forecast10day/q/autoip.json"
+  weatherCall(urls);
+}
+let clearTable = () => {
+  $('#currentTable').empty();
+}
+
+let startSearch = () => {
+  let selectVal = $( "#myselect" ).val();
+  let queryCity = document.getElementById('city').value;
+  let queryState = document.getElementById('state').value;
+  let queryZip = document.getElementById('zipcode').value;
+  let urls = '';
+  let zipOrCity = false;
   if (!queryZip) {
     urls = "http://api.wunderground.com/api/0c6827c56d281db1/forecast10day/q/" + queryState + "/" + queryCity + ".json"
     zipOrCity = true;
@@ -23,11 +32,15 @@ var weatherCall = function(urls, zipOrCity) {
   weatherCall(urls, zipOrCity);
 };
 
-var addToDom = function(result) {
-  var today;
-  var forcastfortheday;
-  var info = result.forecast.txt_forecast.forecastday
-  for (var i = 0; i < 8; i+=2) {
+let addToDom = result => {
+  let today;
+  let forcastfortheday;
+  let date;
+  clearTable();
+  createTable();
+  let info = result.forecast.txt_forecast.forecastday
+  console.log(info.length)
+  for (let i = 0; i < 8; i+=2) {
     date = info[i].title;
     today =  info[i].icon_url;
     forcastfortheday = info[i].fcttext;
@@ -36,3 +49,8 @@ var addToDom = function(result) {
     $('<th><img src='+ today +'></th>').appendTo('#imginfo');
   }    
 };
+let createTable = () => {
+  $("<tr id='day'></tr>").appendTo('#currentTable');
+  $("<tr id='txtinfo'></tr>").appendTo('#currentTable');
+  $("<tr id='imginfo'></tr>").appendTo('#currentTable');    
+}
